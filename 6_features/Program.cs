9c9602e8ad21c76ebb6bcbd434c19ed779f11f6f
Yulia Miller <miller82.98@mail.ru>
2023-01-10 22:23:02 -0800

    Добавьте файлы проекта.

---
+++ 6_features/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyNamespace
+{
+	class MyClass
+	{
+		static void Main(string[] args)
+		{
+			User user= new User();
+
+			user.Age = 23;
+
+			Console.WriteLine(user.Age);
+		}
+	}
+	
+
+	class User
+	{
+		private int age;
+
+		public int Age
+		{
+			get
+			{
+				return age;
+			}
+
+			set
+			{
+				if(value < 18)
+				{
+					Console.WriteLine("Возраст должен быть не менее 18 лет");
+				}
+				else
+				{
+					age = value;
+				}
+			}
+		}
+	} 
+}
